d790564bb01adc78c2b13156275d48e1bca6ac87
Uros <uros.vukic@outlook.com>
2020-01-18 11:04:08 +0100

    added shell scripts for automation of install and clean process

---
+++ clean.sh
@@ -0,0 +1,10 @@
+sudo apt-get --purge remove linux-image-5.0.1-signals
+sudo apt-get --purge remove linux-headers-5.0.1-signals
+
+cd ~/Linux-kernel-modification/
+
+rm *.gz
+rm *.dsc
+rm *.buildinfo
+rm *.changes
+rm *.deb
---
+++ install.sh
@@ -0,0 +1,4 @@
+cd ~/Linux-kernel-modification/
+
+sudo dpkg -i linux-image-5.0.1*.deb
+sudo dpkg -i linux-headers-5.0.1*.deb
